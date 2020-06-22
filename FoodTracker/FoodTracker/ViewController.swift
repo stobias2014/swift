6c5211f854961327c4276762ef463f72c631110a
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-22 00:28:13 -0500

    added FoodTracker

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Saul Tobias on 6/21/20.
+//  Copyright © 2020 Saul Tobias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
